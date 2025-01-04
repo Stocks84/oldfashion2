a594c25e8d6822fa8318f107ec0ff0f0ee98f341
William Stocks <willstocks@me.com>
2025-01-04 11:07:39 +0000

    Add HomePage structure

---
+++ oldfashion/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>HomePage</h1>
+            <p>Recent uploads of games of games and user information will go here.</p>
+        </div>
+    );
+};
+
+export default HomePage;
