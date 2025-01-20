fd7bcdb9a022a35a93ccdb324c0e399fc9f2c0cc
William Stocks <willstocks@me.com>
2025-01-20 21:16:48 +0000

    Fix urls issue

--- oldfashion/src/services/homeService.js
+++ oldfashion/src/services/homeService.js
@@ -1,8 +1,15 @@
 import API from "./api";
+import axios from 'axios';
+
+const BASE_URL = 'https://drf-old-fashion2-89d0730feda0.herokuapp.com';
 
 // Fetch recent games
+// export const fetchRecentGames = async () => {
+//     const response = await API.get("/games/recent");
+//     return response.data;
+// };
 export const fetchRecentGames = async () => {
-    const response = await API.get("/games/recent");
+    const response = await axios.get(`${BASE_URL}/api/games/recent`);
     return response.data;
 };
 
