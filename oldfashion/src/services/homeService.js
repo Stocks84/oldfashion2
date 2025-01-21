d263aa5c8eab7b6f46add8f9a5a9651651b3c0e9
William Stocks <willstocks@me.com>
2025-01-21 09:58:53 +0000

    Fix issues

--- oldfashion/src/services/homeService.js
+++ oldfashion/src/services/homeService.js
@@ -1,17 +1,33 @@
 import API from "./api";
 import axios from 'axios';
 
-const BASE_URL = 'https://drf-old-fashion2-89d0730feda0.herokuapp.com';
+const BASE_URL = 'https://drf-old-fashion2-89d0730feda0.herokuapp.com/api/games';
 
+const token = localStorage.getItem('token');
+
+const API_URL = 'https://drf-old-fashion2-89d0730feda0.herokuapp.com/api/games';
+
+export const fetchRecentGames = async () => {
+    const token = localStorage.getItem('token');
+    return await axios.get(`${API_URL}/recent/`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+};
 // Fetch recent games
 // export const fetchRecentGames = async () => {
-//     const response = await API.get("/games/recent");
+//     const response = await API.get("/recent");
 //     return response.data;
 // };
-export const fetchRecentGames = async () => {
-    const response = await axios.get(`${BASE_URL}/api/games/recent`);
-    return response.data;
-};
+// export const fetchRecentGames = async () => {
+//     const response = await axios.get(`${BASE_URL}/recent/`, {
+//         headers: {
+//             'Authorization': `Token ${token}`
+//         }
+//     });
+//     return response.data;
+// };
 
 // Fetch user info by userId
 export const fetchUserInfo = async (userId) => {
